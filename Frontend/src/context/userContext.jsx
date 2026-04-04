2c8578956d119726ea4e666292be905be3f34810
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-04-04 20:20:53 +0530

    Update userContext.jsx

--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -1,39 +1,39 @@
-import axios from 'axios';
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { AuthDataContext } from './authContext';
+// import axios from 'axios';
+// import React, { createContext, useContext, useEffect, useState } from 'react';
+// import { AuthDataContext } from './authContext';
 
-export const UserDataContext = createContext();
+// export const UserDataContext = createContext();
 
-export default function UserDataProvider({ children }) {
-  const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
+// export default function UserDataProvider({ children }) {
+//   const [userData, setUserData] = useState(null);
+//   const [loading, setLoading] = useState(true);
 
-  const serverUrl = useContext(AuthDataContext);
+//   const serverUrl = useContext(AuthDataContext);
 
-  const getCurrentUser = async () => {
-    try {
-        console.log("Server URL:", serverUrl);
-      const result = await axios.get(`${serverUrl}/api/user/currentuser`,{ withCredentials: true });
-      setUserData(result.data);
-      console.log(result.data);
+//   const getCurrentUser = async () => {
+//     try {
+//         console.log("Server URL:", serverUrl);
+//       const result = await axios.get(`${serverUrl}/api/user/currentuser`,{ withCredentials: true });
+//       setUserData(result.data);
+//       console.log(result.data);
 
-    } catch (error) {
-      setUserData(null);
-      console.log("Error in getCurrentUser", error);
-    } finally {
-      setLoading(false);   
-    }
-  };
+//     } catch (error) {
+//       setUserData(null);
+//       console.log("Error in getCurrentUser", error);
+//     } finally {
+//       setLoading(false);   
+//     }
+//   };
 
-  useEffect(() => {
-    getCurrentUser();
-  }, [serverUrl]);
+//   useEffect(() => {
+//     getCurrentUser();
+//   }, [serverUrl]);
 
-  return (
-    <UserDataContext.Provider
-      value={{ userData, setUserData, getCurrentUser, loading }}
-    >
-      {children}
-    </UserDataContext.Provider>
-  );
-}
+//   return (
+//     <UserDataContext.Provider
+//       value={{ userData, setUserData, getCurrentUser, loading }}
+//     >
+//       {children}
+//     </UserDataContext.Provider>
+//   );
+// }
