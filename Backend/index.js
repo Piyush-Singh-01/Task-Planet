f6a6c5bafa5efcc6d1b204241032adef58f6a99b
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-04-04 20:07:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const authRouter = require("./routes/authRoute");
 const postRouter = require("./routes/postRoute");
 const userRouter  = require("./routes/userRoute");
-// app.set("trust proxy", 1);  // 🔥 ADD THIS LINE
+app.set("trust proxy", 1); 
 
 app.use(cors({
     origin: "https://task-planet-pi.vercel.app",
